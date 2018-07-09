a7bcd8764e476b897bada6d7f245e3e427b1d34a
Rahul Parhi <rahul@parhi.com>
2018-07-08 22:13:48 -0500

    Add environment variable for dotfile location

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -2,7 +2,7 @@
 
 pkill -x polybar
 
-MONITOR=eDP1 polybar --config=$HOME/.dotfiles/polybar.conf main &
-#MONITOR=DP1-1 polybar --config=$HOME/.dotfiles/polybar.conf main &
-#MONITOR=DP1-2 polybar --config=$HOME/.dotfiles/polybar.conf aux &
+MONITOR=eDP1 polybar --config=$DOTFILES/polybar.conf main &
+#MONITOR=DP1-1 polybar --config=$DOTFILES/polybar.conf main &
+#MONITOR=DP1-2 polybar --config=$DOTFILES/polybar.conf aux &
 
