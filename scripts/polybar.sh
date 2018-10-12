edbcc2e69f58a46beef72f5604f31588650906cf
Rahul Parhi <rahul@parhi.com>
2018-10-12 10:49:02 -0500

    Polybar tweaks

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -3,6 +3,6 @@
 pkill -x polybar
 
 MONITOR=eDP1 polybar --config=$DOTFILES/polybar.conf main &
-#MONITOR=DP1-1 polybar --config=$DOTFILES/polybar.conf main &
-#MONITOR=DP1-2 polybar --config=$DOTFILES/polybar.conf aux &
+MONITOR=DP1-1 polybar --config=$DOTFILES/polybar.conf main &
+MONITOR=DP1-2 polybar --config=$DOTFILES/polybar.conf aux &
 
